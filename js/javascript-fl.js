6545f157728f63e3a5152371f178137fae40e880
Afloatz <florentlaot.fr@gmail.com>
2021-04-22 10:51:56 +0100

    display form on click

--- js/javascript-fl.js
+++ js/javascript-fl.js
@@ -1,3 +1,18 @@
+// Display the ticket order form  when the user clicks on one of the "Buy Now" button
+
+function myPrompt() {
+
+  var ticketOrder = document.getElementById("ticket-order");
+  ticketOrder.style.display = 'block'; // display the ticket order form
+
+  // Scroll down to the ticket order form
+  var elmnt = document.getElementById("ticket-order");
+  elmnt.scrollIntoView();
+}
+
+
+
+
 // Calculate the tickets total price when the user choose the quantity on page tickets.html
 
 $(document).ready(function() {
@@ -19,6 +34,5 @@ $(document).ready(function() {
 
         // Display the total price to the user
         $("#total-price").text(totalPrice + '€');
-
   });
 });
